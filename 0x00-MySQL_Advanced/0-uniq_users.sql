08c4268fb19875e544781e45997927820b0a1e75
yeonoel <yeopevroguinoel@gmail.com>
2023-01-18 13:53:38 +0000

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,9 +2,9 @@
 -- If the table already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS `users` (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+	name VARCHAR(255),
 	PRIMARY KEY(id)
 	);
 

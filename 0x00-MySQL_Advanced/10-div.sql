385bf197f89bcb059c2a287886def10b2c0ec512
yeonoel <yeopevroguinoel@gmail.com>
2023-01-20 16:46:36 +0000

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -7,8 +7,9 @@ RETURN FLOAT DETERMINISTIC
 BEGIN
     IF (b = 0)
     THEN 
-       RETURN (0);
-    END IF
-    RETURN (a / b);
+        RETURN (0);
+    ELSE
+	RETURN (a / b);
+    END IF;
 END $$
 DELIMITER ;

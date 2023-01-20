1f28a91df4c8366d23fd153ddcab03c4cede76b1
yeonoel <yeopevroguinoel@gmail.com>
2023-01-20 12:09:23 +0000

    Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- script that creates a trigger that resets the
 -- attribute valid_email only when the email has been changed
 CREATE TRIGGER `reset_attribut`
-AFTER UPDATE ON users
+AFTER UPDATE users ON users.email
 FOR EACH ROW
 UPDATE users SET valid_email = 0;

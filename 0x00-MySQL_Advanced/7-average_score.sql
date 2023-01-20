de61b79fb4cc25f51a14f8e30d8c2a168515b6eb
yeonoel <yeopevroguinoel@gmail.com>
2023-01-20 15:24:34 +0000

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,5 @@
 -- creates a stored procedure ComputeAverageScoreForUser that computes
---and store the average score for a student An average score can be a decimal
+-- and store the average score for a student An average score can be a decimal
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN

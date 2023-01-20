abdf073a53708628ce36244bc5f3642aa4a243fa
yeonoel <yeopevroguinoel@gmail.com>
2023-01-20 14:31:35 +0000

    add bonus

--- 0x00-MySQL_Advanced/6-main.sql
+++ 0x00-MySQL_Advanced/6-main.sql
@@ -1,47 +1,17 @@
--- Initial
-DROP TABLE IF EXISTS corrections;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS projects;
+-- Show and add bonus correction
+SELECT * FROM projects;
+SELECT * FROM corrections;
 
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    name varchar(255) not null,
-    average_score float default 0,
-    PRIMARY KEY (id)
-);
+SELECT "--";
 
-CREATE TABLE IF NOT EXISTS projects (
-    id int not null AUTO_INCREMENT,
-    name varchar(255) not null,
-    PRIMARY KEY (id)
-);
+CALL AddBonus((SELECT id FROM users WHERE name = "Jeanne"), "Python is cool", 100);
 
-CREATE TABLE IF NOT EXISTS corrections (
-    user_id int not null,
-    project_id int not null,
-    score int default 0,
-    KEY `user_id` (`user_id`),
-    KEY `project_id` (`project_id`),
-    CONSTRAINT fk_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-    CONSTRAINT fk_project_id FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE
-);
+CALL AddBonus((SELECT id FROM users WHERE name = "Jeanne"), "Bonus project", 100);
+CALL AddBonus((SELECT id FROM users WHERE name = "Bob"), "Bonus project", 10);
 
-INSERT INTO users (name) VALUES ("Bob");
-SET @user_bob = LAST_INSERT_ID();
+CALL AddBonus((SELECT id FROM users WHERE name = "Jeanne"), "New bonus", 90);
 
-INSERT INTO users (name) VALUES ("Jeanne");
-SET @user_jeanne = LAST_INSERT_ID();
+SELECT "--";
 
-INSERT INTO projects (name) VALUES ("C is fun");
-SET @project_c = LAST_INSERT_ID();
-
-INSERT INTO projects (name) VALUES ("Python is cool");
-SET @project_py = LAST_INSERT_ID();
-
-
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_c, 80);
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_py, 96);
-
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_c, 91);
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_py, 73);
-
+SELECT * FROM projects;
+SELECT * FROM corrections;

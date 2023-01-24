b16286ed0774f418bdc4e11ef07d20e031498072
yeonoel <yeopevroguinoel@gmail.com>
2023-01-24 17:34:58 +0000

    Update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,6 +7,6 @@ def update_topics(mongo_collection, name, topics):
     mongo_collection.update_many(
             { "name": name },
             { 
-                "$set": { topics: topics }
+                "$set": { "topics": topics }
             }
 )

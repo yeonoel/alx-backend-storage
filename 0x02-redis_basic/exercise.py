2b8b80c49166bb38078e0128dd0e3de6c083b9e8
yeonoel <yeopevroguinoel@gmail.com>
2023-01-27 12:09:18 +0000

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,17 @@ import redis
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """count how many times methods of the Cache class are called."""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
 def call_history(method: Callable) -> Callable:
     """ Decorator to store the history of inputs and
     outputs for a particular function.
